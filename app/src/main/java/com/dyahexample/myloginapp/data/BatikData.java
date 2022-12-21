0ad73c6bd658020ae5da947c8a0459cdef5f0265
Dia Rohma Octavia <120706300+octaviadyah3@users.noreply.github.com>
2022-12-21 14:28:10 +0700

    Menambahkan data class ragam batik

--- app/src/main/java/com/dyahexample/myloginapp/data/BatikData.java
+++ app/src/main/java/com/dyahexample/myloginapp/data/BatikData.java
@@ -1,4 +1,31 @@
 package com.dyahexample.myloginapp.data;
 
 public class BatikData {
+    private static String[] batikName={
+          "Batik Parang Kusumo",
+          "Batik Gentongan",
+          "Batik Sogan",
+          "Batik Mega Mendung",
+          "Batik Simbut",
+          "Batik Kawung",
+          "Batik Geblek Renteng",
+          "Batik Tujuh Rupa",
+          "Batik Kraton",
+          "Batik Pring Sedapur",
+    };
+
+
+    private static String[] batikDeskripsi={
+           "Parang berasal dari kata pereng atau miring. Bentuk motifnya berbentuk seperti huruf 'S' miring berombak memanjang. Motif parang ini tersebar diseluruh jawa, mulai dari Jawa Tengah, Yogyakarta, dan Jawa Barat. Biasanya, perbedaannya hanya terletakpada aksen dari batik motif parang tersebut. Misalkan, di Yogya ada motif Parang Rusak dan Parang Barong, di Jawa Tengah ada Parang Slobog, serta di Jawa Barat ada Parang klisik.",
+           "Batik Gentongan berasal dari Madura ini menggunakan motif abstrak sederhana, tanaman atau kombinasi keduanya. Warna batik Gentongan biasanya mengambil warna terang seperti merah, kuning, hijau, atau ungu. Batik Gentongan sendiri diambil dari gentong, yaitu gerabah yang dipakai sebagai wadah untuk mencelup kain batik pada cairan warna.",
+           "Motif batik Sogan sudah ada sejak zaman nenek moyang orang Jawa beberapa abad lalu. Batik ini, didominasi oleh warna cokelat muda dan memiliki motif yang khas seperti, bunga dengan aksen titik-titik atau lengkungan garis. Dulunya, batik ini dipakai raja-raja di Jawa khususnya keraton kesultanan Solo. Namun, sekarang dapat dipakai oleh siapa saja, baik warga keraton maupun orang biasa.",
+           "Motif batik Mega Mendung cukup sederhana namun memberi kesan mewah. Motif Mendung di langit mega yang berwarna cerah inilah yang membuat batik Mega Mendung sangat cocok dipakai orang tua maupun anak muda, baik perempuan maupun laki-laki.",
+           "Motif batik Simbut berbentuk daun yang menyerupai daun talas. Motif tersebut merupakan motif yang paling sederhana, hanya menyususn dan merapikan satu jenis motif saja. Motif simbut berasal dari suku Badui Pedalaman di Sunda yang kental dengan peradaban lama. Namun, seiring dengan berjalannya waktu, para penduduk badui yang menerima modernitas mengembangkan batik ini di daerah pesisir Banten. Sehingga batik motif Simbut dikenal juga dengan batik Banten.",
+           "Motif batik Kawung terinspirasi buah kolang kaling. Bentuk kolang kaling yang lonjong tersebut disusun empat sisi membentuk lingkaran. Motif Kawung sering diidentikan  dengan motif sepuluh sen kuno, karena bentuknya yang bulat dengan lubang ditengahnya. Motif ini berasal dan berkembang di Jawa Tengah dan Yogyakarta. Biasanya motifnya sama, hanya bedanya pada hiasan atau aksennya saja. Batik ini juga termasuk motif batik Indonesia yang paling banyak dipakai",
+           "Batik geblek renteng merupakan motif batik yang menyerupai bentuk makanan khas Kulon Progo. Bentuknya berupa pola angka 8. Geblek makanan olahan dari singkong dan merupakan satu makan khas yang menjadi identitas Kulon Progo, Sementara renteng berarti rentengan atau ikatan satu sama lain saat digoreng. Motif Geblek Renteng adalah motif batik dari hasil lomba desain batik khas Kulon Progo yang diadakan tahun 2012 lalu.",
+           "Motif batik Tujuh Rupa dari Pekalongan ini sangat kental dengan nuansa alam. Pada umumnya, batik Pekalongan menampilkan bentuk motif bergambar hewan atau tumbuhan. Motif-motif tersebut diambil dari berbagai campuran kebudayaan lokal dan etnis Cina",
+           "Motif batik keraton berasal dari kebudayaan jawa yang kental dengan sistem kekratonan dan kesultanannya. Batik Kraton ini melmabngkan kearifan, kebijaksanaan, dan juga karisma raja-raja jawa. Dulunya, batik asal Yogya ini hanya boleh dipakai oleh warga keraton saja, namun sekarang sudah umum dipakai siapa saja. Ciri motif batik Kraton adalah motif bunga yang simentris atau sayap burung yang dikenal sebagai motif sawatlar. Motif ini bisa dibilang paling banyak dipakai baik oleh orang Indonesia maupun orang luar negeri.",
+           "Motif batik Pring Sedapur memiliki ciri khas yang simpel dan ilegal. Motif yang dipakai adalah motif bambu, sehingga sering juga disebut sebagai batik Pring batik ini tidak hanya indah dalam kesederhanaan motifnya tetapi memiliki filosofi yang sederhana pula. Dimana bambu memberikan makna ketentraman, keteduhan, dan kerukunan selain itu, bambu/pring juga mempunyai filosofi mendalam bagi orang jawa yakni apa saja dalam diri kita haruslah memberikan manfaat bagi orang lain, sejak lahir sampai mati.",
+
+    };
 }
