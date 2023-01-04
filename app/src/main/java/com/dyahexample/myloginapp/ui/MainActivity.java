ebb1fee61ddf7e9699952c9693e10a581e923cfb
Dia Rohma Octavia <120706300+octaviadyah3@users.noreply.github.com>
2023-01-04 11:40:28 +0700

    Teknik batik layout home_kategory

--- app/src/main/java/com/dyahexample/myloginapp/ui/MainActivity.java
+++ app/src/main/java/com/dyahexample/myloginapp/ui/MainActivity.java
@@ -21,24 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        TextView username = (TextView) findViewById(R.id.username);
-        TextView password = (TextView) findViewById(R.id.password);
 
-        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
-
-        //admin and admin
-
-        loginbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
-                    //correct
-                    Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
-                } else
-                    //incorrect
-                    Toast.makeText(MainActivity.this, "LOGIN FAILED !!!", Toast.LENGTH_SHORT).show();
-            }
-        });
 
     }}
 
