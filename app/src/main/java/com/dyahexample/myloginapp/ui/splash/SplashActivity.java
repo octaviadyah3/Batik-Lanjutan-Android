061676b3e22ad58a5402db08ea97828068dfb872
ShilaNadila22 <shilanadila123@gmail.com>
2023-01-11 12:19:12 +0700

    MENAMBAHKAN PROGRES BAR DAN MENYETTING SPLASHSCREEN AGAR OTOMATIS BERPINDAH KE MAINACTIVITY DALAM WAKTU 5 DETIK

--- app/src/main/java/com/dyahexample/myloginapp/ui/splash/SplashActivity.java
+++ app/src/main/java/com/dyahexample/myloginapp/ui/splash/SplashActivity.java
@@ -7,10 +7,20 @@ import android.os.Bundle;
 import com.dyahexample.myloginapp.R;
 
 public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_DURATION_TIME = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ne handler().postDelayed(new Runnable() {
+            @override
+            public void run(){
+                intent i = new intent(splashscreen.this, MainActivity.class);
+                StartActivity(i);
+                finish();
+            }
+        } ,SPLASH_DURATION_TIME)
     }
 }
