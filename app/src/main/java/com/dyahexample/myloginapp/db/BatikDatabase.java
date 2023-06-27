5c6e4e89f47377c8eefe5a8cb557b9940f552957
Dia Rohma Octavia <120706300+octaviadyah3@users.noreply.github.com>
2023-06-27 18:41:52 +0700

    menambahkan BatikDatabase

--- app/src/main/java/com/dyahexample/myloginapp/db/BatikDatabase.java
+++ app/src/main/java/com/dyahexample/myloginapp/db/BatikDatabase.java
@@ -1,6 +1,7 @@
 package com.dyahexample.myloginapp.db;
 
 import androidx.room.Database;
+import androidx.room.RoomDatabase;
 
 @Database(entities = {Batik.class}, version = 1)
 public abstract class BatikDatabase extends RoomDatabase {
